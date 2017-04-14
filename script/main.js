fd0e689692b0b0991e56c23162551281c24fc306
LyricLiu <yichiliu@foxmail.com>
2017-04-14 17:55:15 -0400

    fix add day bug
    chat window bug unfinished

--- script/main.js
+++ script/main.js
@@ -55,22 +55,24 @@
         this.words = words;
     }
 
-    var g1_0 = 'Congratulations, my friend! You are now the owner of this fantastic shop! I know you are excited and wanna try it now. But before you serve the first guest, let me teach you the simplest way of brew the tea! Click the button below to start!';
-    var g2_0 = "Hi! I am Alice, I come from Switzerland where I have a pretty farm with cows and chickens. It's the first time for me to taste the tea, I am very excited! I don't have any preference, just show me what's popular in your shop.";
-    var g2_1 = "You: Hi, Alice! I am glad to meet you! You are my first guest!";
-    var g2_2 = "Alice: Oh, I am so lucky! You know today is my final day here, so I was wondering where to spend this most precious afternoon. Then I though I should experience tea culture since I am here and I found your tea shop. It’s so great, I like everything here, the furniture, the aroma …";
-    var g2_3 = "You: Thank you! Actually, I am just a seven day owner. But I will try my best to improve this tiny space. Do you have any suggestions?";
-    var g2_4 = "Alice: I am not a tea expert, but there is a market I want to recommend. I think you could but more tea sets from there, that would improve your business …";
-    var g3_0 = "Qin: It seems like here we have a new shop owner! Hi, I am Qin, an old customer of this tea shop. I own a silk shop across the street. Whenever there is no customer, I would come to this tea shop and have my favorite Puer tea. I am glad I will have a new flavor today. You are a novel, right? ";
-    var g3_1 = "You: Thanks for visiting! Yes, I am new here. Hope you don’t mind I have almost no tea knowledge. Why Pu’er is your favorite tea?";
-    var g3_2 = "Qin: You know I have to stay in my shop all day, so I feel tired sometimes. Pu’er tea has a stronger flavor than other tea which helps me reduce plaque. Also, it lowers the risk of heart disease and diabetes. It’s healthy. ";
-    var g3_3 = "You: Oh, that’s great! It sounds like you know a lot about tea. Can you give me any suggestion to improve my brewing skill?";
-    var g3_4 = "Qin: Definitely, let’s brew a tea, I will tell you how to do it. But, firstly, you need to buy a pitcher in the tea shop!";
-    var g1 = new guest(['Fernando'], false, 0, g1_0, []);
-    var g2 = new guest(['Alice'], true, 0, g2_0, [g2_1, g2_2, g2_3, g2_4]);
-    var g3 = new guest(['Qin', 'Lily'], true, 1, g3_0, [g3_1, g3_2, g3_3, g3_4]);
+    var g0_0 = 'Congratulations, my friend! You are now the owner of this fantastic shop! I know you are excited and wanna try it now. But before you serve the first guest, let me teach you the simplest way of brew the tea! Click the button below to start!';
+    var g1_0 = "Hi! I am Alice, I come from Switzerland where I have a pretty farm with cows and chickens. It's the first time for me to taste the tea, I am very excited! I don't have any preference, just show me what's popular in your shop.";
+    var g1_1 = "You: Hi, Alice! I am glad to meet you! You are my first guest!";
+    var g1_2 = "Alice: Oh, I am so lucky! You know today is my final day here, so I was wondering where to spend this most precious afternoon. Then I though I should experience tea culture since I am here and I found your tea shop. It’s so great, I like everything here, the furniture, the aroma …";
+    var g1_3 = "You: Thank you! Actually, I am just a seven day owner. But I will try my best to improve this tiny space. Do you have any suggestions?";
+    var g1_4 = "Alice: I am not a tea expert, but there is a market I want to recommend. I think you could but more tea sets from there, that would improve your business …";
+    var g2_0 = "Qin: It seems like here we have a new shop owner! Hi, I am Qin, an old customer of this tea shop. I own a silk shop across the street. Whenever there is no customer, I would come to this tea shop and have my favorite Puer tea. I am glad I will have a new flavor today. You are a novice, right? ";
+    var g2_1 = "You: Thanks for visiting! Yes, I am new here. Hope you don’t mind I have almost no tea knowledge. Why Pu’er is your favorite tea?";
+    var g2_2 = "Qin: You know I have to stay in my shop all day, so I feel tired sometimes. Pu’er tea has a stronger flavor than other tea which helps me reduce plaque. Also, it lowers the risk of heart disease and diabetes. It’s healthy. ";
+    var g2_3 = "You: Oh, that’s great! It sounds like you know a lot about tea. Can you give me any suggestion to improve my brewing skill?";
+    var g2_4 = "Qin: Definitely, let’s brew a tea, I will tell you how to do it. But, firstly, you need to buy a pitcher in the tea shop!";
+    var g0 = new guest(['Fernando'], false, 0, g0_0, []);
+    var g1 = new guest(['Alice'], true, 0, g1_0, [g1_1, g1_2, g1_3, g1_4]);
+    var g2 = new guest(['Qin', 'Lily'], true, 1, g2_0, [g2_1, g2_2, g2_3, g2_4]);
 
-    var Guest = [g1, g2, g3];
+    var Guest = [g0, g1, g2];
+
+    var iclick = 0;
 
     function lockOn() {
         var i;
@@ -125,7 +127,7 @@
         } else {
             $('#level').html("Expert");
         }
-        $('#money').html(money);
+        $('#money').html(guestIndex);
     }
 
     function setMoneyPic() {
@@ -211,10 +213,10 @@
     }
 
     function addWaterLeaves() {
-        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_2.png)'); }, 300);
-        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_3.png)'); }, 600);
-        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_4.png)'); }, 900);
-        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot.png)'); }, 1200);
+        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_2.png)'); }, 200);
+        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_3.png)'); }, 400);
+        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot_4.png)'); }, 600);
+        setTimeout(function() { $('#pot').css('background-image', 'url(./img/pot.png)'); }, 800);
         if (guestIndex == 0) {
             $('#guild3').css('display', 'none');
             $('#guild4').css('display', 'block');
@@ -276,47 +278,57 @@
 
     function getMoney() {
         $('#cup').attr('drag', 'false');
-        $('#seat1_money').click(function() {
-            seat1_mg = false;
-            money += teaPrice;
-            setPara();
-            $('#seat1_money').css('display', 'none');
-            if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
-                brewInit();
-                guestIntro();
-            }
-        });
-        $('#seat2_money').click(function() {
-            seat2_mg = false;
-            money += teaPrice;
-            setPara();
-            $('#seat2_money').css('display', 'none');
-            if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
-                brewInit();
-                guestIntro();
-            }
-        });
-        $('#seat3_money').click(function() {
-            seat3_mg = false;
-            money += teaPrice;
-            setPara();
-            $('#seat3_money').css('display', 'none');
-            if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
-                brewInit();
-                guestIntro();
-            }
-        });
-        $('#seat4_money').click(function() {
-            seat4_mg = false;
-            money += teaPrice;
-            setPara();
-            $('#seat4_money').css('display', 'none');
-            if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
-                brewInit();
-                guestIntro();
-            }
-        })
     }
+
+    $('#seat1_money').on("click", function() {
+        if (seat1_mg == true) {
+            money += teaPrice;
+            setPara();
+        }
+        seat1_mg = false;
+        $('#seat1_money').css('display', 'none');
+        if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
+            restart = true;
+            brewInit();
+            guestIntro();
+        }
+    });
+    $('#seat2_money').on("click", function() {
+        if (seat2_mg == true) {
+            money += teaPrice;
+            setPara();
+        }
+        seat2_mg = false;
+        $('#seat2_money').css('display', 'none');
+        if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
+            brewInit();
+            guestIntro();
+        }
+    });
+    $('#seat3_money').on("click", function() {
+        if (seat3_mg == true) {
+            money += teaPrice;
+            setPara();
+        }
+        seat3_mg = false;
+        $('#seat3_money').css('display', 'none');
+        if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
+            brewInit();
+            guestIntro();
+        }
+    });
+    $('#seat4_money').on("click", function() {
+        if (seat4_mg == true) {
+            money += teaPrice;
+            setPara();
+        }
+        seat4_mg = false;
+        $('#seat4_money').css('display', 'none');
+        if ((seat1_mg == false) && (seat2_mg == false) && (seat3_mg == false) && (seat4_mg == false)) {
+            brewInit();
+            guestIntro();
+        }
+    })
 
     /******************* Game Section **********************/
     function brewInit() {
@@ -327,6 +339,7 @@
             $('#guild5').css('display', 'none');
         }
         timeNow[1] += 1;
+        setHeader();
         guestIndex += 1;
     }
 
@@ -420,8 +433,49 @@
         }
     }
 
+    /****************** chat section  *******************/
+    function showChat() {
+        if (iclick == 1) {
+            $('#chat_content').html(Guest[guestIndex].words[1]);
+        } else if (iclick == 2) {
+            $('#chat_content').html(Guest[guestIndex].words[2]);
+        } else if (iclick == 3) {
+            $('#chat_content').html(Guest[guestIndex].words[3]);
+            $('#chat_brew').css('display', 'block');
+            if (guestIndex == 2) {
+                //add open shop window;
+            }
+        }
+    }
+
+    $('#chat_window').on("click", function() {
+        iclick += 1;
+        showChat();
+    })
+
+    $('#chat_brew').on("click",function() {
+        iclick = 0;
+        $('#chat_brew').css('display', 'none');
+        $("#chat_window").css('display', 'none');
+        brew();
+    });
+
+    function guestChat() {
+        $('#chat_window').css('display', 'block');
+        var picg = 'url(./img/g' + guestIndex + '.png)';
+        $('#chat_pic').css("background-image", picg);
+        $('#chat_content').html(Guest[guestIndex].words[0]);
+    }
+
 
     /***************** Guest Intro ********************/
+
+    $('#intro_chat').on("click", function() {
+        $("#guest_window").css('display', 'none');
+        timeNow[1] += 1;
+        setHeader();
+        guestChat();
+    })
 
     function guestIntro() {
         $('#guild5').css('display', 'none');
@@ -445,11 +499,6 @@
             //brewProcess(); --- the first of brew tea
             $("#guest_window").css('display', 'none');
         });
-        $('#intro_chat').click(function() {
-            guestChat();
-            $("#guest_window").css('display', 'none');
-            timeNow[1] += 1;
-        })
     }
 
     guestIntro();
