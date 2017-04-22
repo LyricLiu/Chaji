5e8335f3fdfb8d3a7fbc4fb40b26bfcb7c5df303
LyricLiu <yichiliu@foxmail.com>
2017-04-21 23:10:53 -0400

    add all character
    need a end state

--- script/main.js
+++ script/main.js
@@ -107,6 +107,71 @@
     var g9_2 = "That’s the greatest news I heard this year! I am proud of you! Is there anything I can do for you?";
     var g9_3 = "Thanks a lot, Joanna! The biggest problem I face now is that I need to earn 4000 coins in five days which is stressful. Except for that, I am doing everything good!";
     var g9_4 = "Oh, that’s stressful. No worries, My friends and I want to buy some tea leaves home, we can buy from you! Here is 1000 coins, let me taste your work!";
+    var g10_0 = "Hi, my name is Lucy, I would like to have a cup of tea that can promote weight loss. I hope to be slim and I heard that drinking specific tea can help me achieve that. Could you brew that kind of tea to me, thanks!";
+    var g10_1 = "Hello, Lucy. Let’s be honest. There is no magic herb growing on a rock mountainside in some remote part of the world that, on its own, will slim you down until you’re a trim, glowing version of yourself.";
+    var g10_2 = "Oh, you made me disappointed.";
+    var g10_3 = "That’s being said, Oolong can assist in fat burning. Do you wanna try it?";
+    var g10_4 = "Okay, why not?";
+    var g11_0 = "Oh, what a cozy tea shop! Hi, I am John. I’m from American West. I had grown up on a ranch in South Dakota, where I developed an old west, cowboy swagger. Sometimes I drink tea, but I prefer coffee. I am wondering it is because I had never had true tea. Could you let me taste a real Chinese tea?";
+    var g11_1 = "Absolutely. I feel the same way as you before I came here. The only thing I knew is to brew the leaves with hot water. I though it is the right process and enjoyed it.";
+    var g11_2 = "How long have you been here?";
+    var g11_3 = "A couple days. Actually, i am only a 5 days tea shop owner. But I am considering to run my own teashop after this.";
+    var g11_4 = "That will cost a bunch of money. I guess you have to prepare 4000 more coins.";
+    var g12_0 = "Hello! I am Lan. I am a writer. My grand father is a famous master of tea. So I have tasted a great amount of tea in my childhood. I wrote all my experience of tea drinking down and published them as a book. Now I am traveling to seek different flavor. Could you please brew a cup of Oolong tea for me?";
+    var g12_1 = "Hello, Lan, I’m gland to meet you! I have served a lot of guests. A lot of mentioned the experience of tea drinking, but you are the only one that take it so serious! Could you teach me how to enrich the flavor of the tea!";
+    var g12_2 = "Absolutely, do you know what is a good tea?";
+    if (level == 2){
+        var g12_2 = "Do you know you need to warm tea sets before you brew the tea?";
+    }else if (level == 3 || level ==4){
+        var g12_2 = "Do you know why you need warm tea sets?";
+    }
+    var g12_3 = "Em … I am not sure.";
+    var g12_4 = "Okay, as you see, all the tea sets are pretty small. Following the warming process, the tea inside will remain hot longer as it sits, and the warmth of the pot will cause the leaves to release a burst of fragrance. ";
+    if (level == 0 || level == 1 || level == 2){
+        var g12_4 = "- I think the answer is quiet simple’ you explain, ‘it should be memorable. If you are still thinking about it after you finish drinking it. It must be pretty good.";
+    }
+    var g13_0 = "Hi! I am Liu. I am a fever of tea. I have observed several of your guests. They all don’t know how to taste the tea. Fine tea resist easy appreciation. Anyway, may I have a cup of Puer? ";
+    var g13_1 = "Hi, Mr Liu. Why you think all of my guests don’t know how to taste tea?";
+    var g13_2 = "They all in a hurry to drink it. Actually, the taste of tea start from the moment you get tea leaves out of the box. We should appreciate the aroma of these dry leaves. Be quiet, smell it and savor the moment.";
+    var g13_3 = "Oh, it’s interesting.";
+    var g13_4 = "Yeah, when the tea is ready, we should note the color and fragrance of the liquid and then our drink should be deliberate and thoughtful.";
+    if(level == 3){
+        var g13_4 = "Yeah, when the tea is ready, we should note the color and fragrance of the liquid and then our drink should be deliberate and thoughtful. Buy a smelling cup from the shop, you need this to help you better taste the tea.";
+    }
+    var g14_0 = "Nice to meet you, I am Anna. I forgot the name of tea I want to drink, I remember the color is red. Could you brew that kind of tea for me, thanks!";
+    var g15_0 = "Hi, I am Shan. May I have a cup of Long Jing tea. Hurry up please, I only have ten minutes.";
+    var g15_1 = "Sorry Mr. Shan. It takes time to brew a good tea.";
+    var g15_2 = "Oh. That sucks. How long does it take for steeping?";
+    var g15_3 = "It’s not only steeping. I have other processes to go before brew the tea.";
+    var g15_4 = "Okay, okay, at least, please do it very quick.";
+    var g16_0 = "Hello, I am Kimmy. I am from Korean. My favorite tea is Dohwa-cha which is a tea made from dried peach blossoms. Do you serve this in your shop? If not, may I have a cup of regular green tea?";
+    var g16_1 = "Nice to meet you, Kimmy. I apologize I don’t have flower tea in my shop.";
+    var g16_2 = "No worries, I heard that Chinese people do not drink flower tea and fruit tea, is it true?";
+    var g16_3 = "Not at all. chrysanthemum tea is popular here!";
+    var g16_4 = "Oh, we drink this, too …";
+    var g17_0 = "Hello, I am Wei Sheng. I am a tea farmer from a small village in Sichuan which is famous for Puer tea. I am on my journey along Yangtze river for Puer trade now. I had a long time not drink Pure tea. I really miss it. Could you brew Puer tea for me?";
+    var g17_1 = "Hello, Mr Wei. I am so glad to meet you! I have a lot of questions for you about tea! How is tea made in your village?";
+    var g17_2 = "You know, Chinese tea is mostly small-scale village handicraft. So during the tea harvest, we select and pinch off tea leaves, then we heated tea leaves with charcoal in fiery woks and then vigilantly tended piles of oxidizing leaves.";
+    var g17_3 = "So, you just heated the leaves once?";
+    var g17_4 = "No, we need to periodical heat these leaves and make them dry. It takes a long time before we can pack and sell them …";
+    var g18_0 = "Hi, It’s good to know Mr.Li recruited a new shop owner! I am Xiangxiang. I used to be the owner of this tea shop. That’s three years ago. Today, I happened to past this tea shop and I am curious that who is the shop owner now. Could you please brew my favorite Puer tea for me today?";
+    var g18_1 = "Hi, thanks for coming! It’s amazing to know you used to own this tea shop! I think I could know a lot from you!";
+    var g18_2 = "Haha~ You know, I owned this shop for five days as you, and I had earned 9K coins eventually!";
+    var g18_3 = "That’s so amazing! I cannot wait to learn something from you!";
+    var g18_4 = "No problem, click the brew button and let me show you how I succeed in this business!";
+    if (level ==4){
+        var g18_4 = "Oh, no, you are already an expert! That is me that should learn from you!";
+    }
+    var g19_0 = "Hello, I am Tian Tian. I heard that your tea shop is great. I want to have a cup of Long Jing tea. Thanks!";
+    var g19_1 = "Hi, Tian Tian, thanks for coming! How you heard from my tea shop.";
+    var g19_2 = "One of my friend recommend your shop to me.";
+    var g19_3 = "Oh, that’s cool. Please recommend my shop to your friend if you like.";
+    var g19_4 = "No problem.";
+    var g20_0 = "What an exquisite kettle, where did you get it? Hi, I am Lin Yu. I am a teacher worked in local primary school. I like to spend my free time on appreciating tea. The most important reason is that I love beautiful tea set! May I have a cup of Oolong, please!";
+    var g20_1 = "Welcome Mr. Lin. That is to say you are an expert of tea set! I am excited! Before I knew you, I have never cast an eye on my tea set! Tell me more about these tea sets please!";
+    var g20_2 = "Okay, look at this very small purple sand clay pot you use to brew the tea here! This is from Yixing which is a historic city in the Yangtze River basin long famous for its ceramics.";
+    var g20_3 = "Oh, this is fantastic. But wait, it seems no glaze on it.";
+    var g20_4 = "You are right. This purple sand clay is so pleasant to touch that the pot does not need a glaze…";
 
 
     var g0 = new guest(['Mr.Li'], false, 3, g0_0, []);
@@ -119,9 +184,20 @@
     var g7 = new guest(['Ms.Yu', 'Ms.Ling ', "Ms.Hu"], true, 2, g7_0, [g7_1, g7_2, g7_3, g7_4]);
     var g8 = new guest(['Mr.Chen', 'Ms.Qin ', "Mr.Zhang"], true, 1, g8_0, [g8_1, g8_2, g8_3, g8_4]);
     var g9 = new guest(['Joanna', 'Christine ', "Stephanie", "Mariam"], true, 3, g9_0, [g9_1, g9_2, g9_3, g9_4]);
+    var g10 = new guest(['Lucy', 'Betty', "Cherry", "Linda"], true, 2, g10_0, [g10_1, g10_2, g10_3, g10_4]);
+    var g11 = new guest(['John', 'Hayden', "Michelle", "Jemma"], true, 3, g11_0, [g11_1, g11_2, g11_3, g11_4]);
+    var g12 = new guest(['Lan', 'Jun', "Xiaoxue", "Xiaoxi"], true, 2, g12_0, [g12_1, g12_2, g12_3, g12_4]);
+    var g13 = new guest(['Mr.Liu', 'Ms.Xia', "Mr.Qin", "Ms.Qin"], true, 1, g13_0, [g13_1, g13_2, g13_3, g13_4]);
+    var g14 = new guest(['Anna'], false, 1, g14_0, []);
+    var g15 = new guest(['Mr.Shan', 'Ms.Lin', "Mr.Qin", "Ms.Zhao"], true, 0, g15_0, [g15_1, g15_2, g15_3, g15_4]);
+    var g16 = new guest(['Kimmy', 'Kim', "Lawe", "Uno"], true, 3, g16_0, [g16_1, g16_2, g16_3, g16_4]);
+    var g17 = new guest(['Mr.Wei', 'Mr.Bai', "Ms.Lee", "Mr.Xu"], true, 1, g17_0, [g17_1, g17_2, g17_3, g17_4]);
+    var g18 = new guest(['Xiangxiang', 'Rose', "Petro", "Glandia"], true, 1, g18_0, [g18_1, g18_2, g18_3, g18_4]);
+    var g19 = new guest(['Tiantian', 'Lily', "Shruti", "Andrew"], true, 0, g19_0, [g19_1, g19_2, g19_3, g19_4]);
+    var g20 = new guest(['Mr.Lin', 'Lufeng', "Jessica", "Lokhin"], true, 2, g20_0, [g20_1, g20_2, g20_3, g20_4]);
 
 
-    var Guest = [g0, g1, g2, g3, g4, g5, g6, g7, g8, g9];
+    var Guest = [g0, g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20];
 
     var iclick = 0;
     var brew2_first = true;
@@ -242,9 +318,9 @@
     })
 
     $('#sell4').on("click", function() {
-        if (money >= 150) {
+        if (money >= 200) {
             seat2.lock = false;
-            money -= 150;
+            money -= 200;
             setPara();
             lockOn();
             $('#sell4h').css('display', 'block');
@@ -252,9 +328,9 @@
     })
 
     $('#sell5').on("click", function() {
-        if (money >= 350 && seat2.lock == false) {
+        if (money >= 500 && seat2.lock == false) {
             seat3.lock = false;
-            money -= 350;
+            money -= 500;
             setPara();
             lockOn();
             $('#sell5h').css('display', 'block');
@@ -262,41 +338,41 @@
     })
 
     $('#sell6').on("click", function() {
-        if (money >= 800  && seat2.lock == false && seat3.lock == false) {
+        if (money >= 1000  && seat2.lock == false && seat3.lock == false) {
             seat4.lock = false;
-            money -= 800;
+            money -= 1000;
             setPara();
             lockOn();
-            teaPrice += 150;
+            teaPrice += 100;
             $('#sell6h').css('display', 'block');
         }
     })
 
     $('#sell7').on("click", function() {
-        if (money >= 1000) {
-            money -= 1000;
+        if (money >= 2000) {
+            money -= 2000;
             dessert.lock = false;
             setPara();
             lockOn();
-            teaPrice += 300;
+            teaPrice += 200;
             $('#sell7h').css('display', 'block');
         }
     })
 
     $('#sell8').on("click", function() {
-        if (money >= 600) {
-            money -= 600;
+        if (money >= 800) {
+            money -= 800;
             //change picture;
             setPara();
             lockOn();
-            teaPrice += 100;
+            teaPrice += 70;
             $('#sell8h').css('display', 'block');
         }
     })
 
     $('#sell9').on("click", function() {
-        if (money >= 400) {
-            money -= 400;
+        if (money >= 600) {
+            money -= 600;
             smelling.lock = false;
             setPara();
             lockOn();
@@ -306,12 +382,12 @@
     })
 
     $('#sell10').on("click", function() {
-        if (money >= 2000) {
-            money -= 2000;
+        if (money >= 4000) {
+            money -= 4000;
             //change picture;
             setPara();
             lockOn();
-            teaPrice += 800;
+            teaPrice += 300;
             $('#sell9h').css('display', 'block');
         }
     })
@@ -349,7 +425,6 @@
             $('#sell7').css('display', 'block');
             $('#sell8').css('display', 'block');
             $('#sell9').css('display', 'block');
-            $('#sell10').css('display', 'block');
         }
     }
 
@@ -897,6 +972,7 @@
                     setShopLock();
                 }
                 setPara();
+                addHour();
             }
             if (guestIndex == 3) {
                 level += 1;
@@ -906,6 +982,7 @@
                     setShopLock();
                 }
                 setPara();
+                addHour();
             }
             if (guestIndex == 7) {
                 level += 1;
@@ -915,6 +992,7 @@
                     setShopLock();
                 }
                 setPara();
+                addHour();
             }
             if(guestIndex == 9){
                 money += 1000;
@@ -922,6 +1000,36 @@
                 $('#teaP').html('1000');
                 setPara();
                 setTimeout(function() { $('#money_pop').fadeOut(500); }, 900)
+            }
+            if (guestIndex == 12) {
+                if(level == 2){
+                    level += 1;
+                    teaPrice += 50;
+                    setPara();
+                    addHour();
+                }
+            }
+            if (guestIndex == 13) {
+                if(level == 3){
+                    level += 1;
+                    teaPrice += 50;
+                    $('#shop_window').css('display', 'block');
+                    setShopLock();
+                    setPara();
+                    addHour();
+                }
+            }
+            if (guestIndex == 18) {
+                if (level < 4){
+                    level += 1;
+                    teaPrice += 50;
+                    if (level == 1 || level == 4){
+                        $('#shop_window').css('display', 'block');
+                        setShopLock();
+                    }
+                setPara();
+                addHour();
+                }
             }
         }
     })
